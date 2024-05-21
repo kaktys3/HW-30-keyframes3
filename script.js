faa0e8447e19cb06d2403c8212839f333d0b3dde
kaktys3 <tasianl2015@gmail.com>
2024-05-21 08:20:52 +0300

    HW-30-keyframes3

---
+++ script.js
@@ -0,0 +1,17 @@
+// /*помятай let завжди вище alert*/
+
+// let HelloUser = "idi nahui h suka";
+
+// alert (HelloUser);
+
+// let HelloUser2 = "jhhjfhvhvvv";
+
+// console.log (HelloUser2);
+
+// // stric
+// const HelloUser3 = "hello user";
+// console.log (typeof HelloUser3);
+
+// // namber
+// const num = 18786;
+// console.log (typeof num);
